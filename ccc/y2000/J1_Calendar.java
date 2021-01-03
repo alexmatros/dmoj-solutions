25ac9fd4307c1e4727b4b4443b0e6fa692faf5df
Alex M <alex@matros.ca>
2021-01-03 18:46:34 -0500

    ccc 00 j1 - calendar

--- ccc/y2000/J1_Calendar.java
+++ ccc/y2000/J1_Calendar.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class J1 {
+public class J1_Calendar {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
